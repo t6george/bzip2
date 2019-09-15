9b99ceb9feef4958be201b211d6b6c2a6c315985
ThomasGeorge <t6george@edu.uwaterloo.ca>
2019-09-15 18:20:11 -0400

    moss

--- bzip2.pl
+++ bzip2.pl
@@ -1,6 +1,8 @@
 use warnings;
 use strict;
 
+use constant BWT_BLOCK_SIZE => 960 * 1000;
+
 open(FH, '<', './sample2.txt') or die $!;
 my $fileString = <FH>;
 my @fileText = split //, $fileString;
@@ -11,6 +13,11 @@ my @rleEncoding = ();
 my $runLength = 1;
 
 for my $i ((1..$#fileText)) {
+
+  # if (($i + 1) % BWT_BLOCK_SIZE == 0) {
+  #   push @rleEncoding, "\$";
+  # }
+
   if ($fileText[$i-1] eq $fileText[$i]) {
     ++$runLength;
   } else {
@@ -24,10 +31,34 @@ for my $i ((1..$#fileText)) {
   }
 }
 
+push @rleEncoding, "\$";
+
 my $rle = join "", @rleEncoding;
 print "After RLE:\n" . $rle . "\n";
 
 # BWT
+
+my %suffixMap = ();
+my @suffixArray = ();
+
+for my $i ((0..(length $rle) - 1)) {
+  $suffixMap{substr $rle, $i, ((length $rle) + 1 - $i)} = $i;
+}
+
+for (sort (keys %suffixMap)) {
+  push @suffixArray, %suffixMap{$_};
+}
+
+my @bwtEncoding = ();
+
+print %suffixMap;
+
+# for my $i ((0..$#suffixArray)) {
+#   push @bwtEncoding, (substr $rle, ($suffixArray[$i] + (length $rle) - 1) % (length $rle), 1);
+# }
+
+# my $bwt = join "", @bwtEncoding;
+# print "After BWT:\n" . $bwt . "\n";
 
 
 
